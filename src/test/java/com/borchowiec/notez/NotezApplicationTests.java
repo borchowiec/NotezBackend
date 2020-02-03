5436351478e95cea89084acbb49cebf31800d578
borchowiec <borchowiecpatryk@gmail.com>
2020-02-03 10:46:18 +0100

    Init commit

---
+++ src/test/java/com/borchowiec/notez/NotezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borchowiec.notez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
