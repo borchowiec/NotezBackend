7f106ca5f486c42bd74b8421228e67024e36e011
borchowiec <borchowiecpatryk@gmail.com>
2020-02-29 21:04:21 +0100

    Added plalist entity

---
+++ src/main/java/com/borchowiec/notez/model/Playlist.java
@@ -0,0 +1,59 @@
+package com.borchowiec.notez.model;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Playlist {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @OneToMany
+    private List<Song> songs;
+
+    @CreatedBy
+    private long owner;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public long getOwner() {
+        return owner;
+    }
+
+    public void setOwner(long owner) {
+        this.owner = owner;
+    }
+}
